bb906a762c5135b92f1713b2cfeedcdcf52e14b2
Nabarun21 <ndev@nd.edu>
2019-02-09 10:12:54 -0500

    add cleaned version

---
+++ thesis/clean.sh
@@ -0,0 +1,8 @@
+rm thesis_v0.log
+rm thesis_v0.toc
+rm thesis_v0.lof
+rm thesis_v0.lot
+rm thesis_v0.aux
+rm thesis_v0.tes
+rm thesis_v0.bbl
+rm thesis_v0.blg
---
+++ thesis/compile.sh
@@ -0,0 +1,5 @@
+pdflatex thesis_v0
+bibtex thesis_v0
+pdflatex thesis_v0
+pdflatex thesis_v0
+
